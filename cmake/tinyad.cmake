9eab52408889fc048d164190585124e7a7d08b2b
thismachinewritesthesis <josh@foolishproducts.com>
2024-01-02 16:29:22 -0600

    fix annoying issue

--- cmake/tinyad.cmake
+++ cmake/tinyad.cmake
@@ -1,8 +1,10 @@
 include(FetchContent)
 FetchContent_Declare(
   tinyad
-  GIT_REPOSITORY git@github.com:the13fools/gpgpt.git
-  GIT_TAG 770dc5011371787ef784019f62794ec560623f79
+  GIT_REPOSITORY https://github.com/the13fools/TinyAD.git
+  # GIT_TAG 770dc5011371787ef784019f62794ec560623f79
+  GIT_TAG v1.0
+  # GIT_TAG 770dc5011371787ef784019f62794ec560623f79
 )
 FetchContent_GetProperties(tinyad)
 if(NOT tinyad_POPULATED)
