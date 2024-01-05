1253f7344c1c798e535fca80287c8a73536020a8
thismachinewritesthesis <josh@foolishproducts.com>
2024-01-05 13:09:37 -0600

    small tweaks

--- cmake/polyscope.cmake
+++ cmake/polyscope.cmake
@@ -14,3 +14,26 @@ if(NOT polyscope_POPULATED)
   add_subdirectory(${polyscope_SOURCE_DIR} ${polyscope_BINARY_DIR})
 endif()
 FetchContent_MakeAvailable(polyscope)
+
+
+# for now just manually include implot in this gpgpt
+
+# # Set the IMGUI_INCLUDE_DIR
+# set(IMGUI_INCLUDE_DIR ${polyscope_SOURCE_DIR}/deps/imgui)
+
+# # Fetch implot
+# FetchContent_Declare(
+#     implot
+#     GIT_REPOSITORY https://github.com/epezent/implot.git
+#     GIT_TAG v0.16
+# )
+# FetchContent_GetProperties(implot)
+# if(NOT implot_POPULATED)
+#   FetchContent_Populate(implot)
+#   add_subdirectory(${implot_SOURCE_DIR} ${implot_BINARY_DIR})
+# endif()
+
+# # Here, adjust the include directories for implot if needed
+# target_include_directories(implot PUBLIC ${IMGUI_INCLUDE_DIR})
+
+
